609740f786b49ec9fe2a45b69f027148847cb888
Daniel Adeboye <oluwadaunsid@gmail.com>
2023-11-29 01:23:07 +0100

    created portfolio website

--- src/portfolio.js
+++ src/portfolio.js
@@ -6,14 +6,14 @@ const header = {
 
 const about = {
   // all the properties are optional - can be left empty or deleted
-  name: 'John Smith',
-  role: 'Front End Engineer',
+  name: 'Daniel Adeboye',
+  role: 'AI Engineer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+    "My journey has been fueled by a love for mathematics, science, and the limitless potential of artificial intelligence. I hold a distinguished first-class honors degree in Electrical Engineering. 🔭 I’m currently working on developing AI applications to revolutionize health care, diagnostics, and treatment methodologies.👯 I’m looking to collaborate on research centered around transformer architectures, image segmentation, and generative models.⚡ Fun fact: When I'm not immersed in the world of AI, you can find me engrossed in amazing books like The Bible, Thinking Fast, and Slow, Movies have a special place in my heart, especially those that bring true stories to life on the big screen. I cherish my quiet moments, during which I ponder the vast tapestry of past and future possibilities. Exploring the fate of the world and its intricate complexities fuels my curiosity and creativity.",
   resume: 'https://example.com',
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://linkedin.com/in/daniel-adeboye/',
+    github: 'https://github.com/daunsid',
   },
 }
 
@@ -49,23 +49,23 @@ const projects = [
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-  'HTML',
-  'CSS',
-  'JavaScript',
-  'TypeScript',
-  'React',
-  'Redux',
-  'SASS',
-  'Material UI',
-  'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'Python',
+  'Golang',
+  'C/C++',
+  'Azure',
+  'AWS',
+  'Ubuntu - Linux',
+  'API',
+  'Machine Learning',
+  'Computer Vision',
+  'Generative AI',
+  'Docker',
+  'MLOPs',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'adeboye.daniel17@mail.com',
 }
 
 export { header, about, projects, skills, contact }
